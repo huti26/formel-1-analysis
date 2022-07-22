4b036b12a9a574255cfd4c0c31637545a19b3860
Hutan <31436976+huti26@users.noreply.github.com>
2022-07-22 23:40:41 +0200

    update

--- racepace_alphabetical.R
+++ racepace_alphabetical.R
@@ -15,40 +15,33 @@ df$delta.seconds <- df$delta / 1000
 df$ham.faster <- df$delta > 0
 
 g <- ggplot(df) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "#999999") +
-    geom_bar(
-    aes(y = delta.seconds, x = race_name, fill = ham.faster),
+  geom_vline(xintercept = 0, linetype = "dashed", color = "#999999") +
+  geom_bar(
+    aes(x = delta.seconds, y = race_name, fill = ham.faster),
     size = 0.7, alpha = 1, color = "black", width = 0.25,
     stat = "identity"
   ) +
-  facet_grid(rows = vars(year)) +
+  facet_grid(cols = vars(year)) +
   labs(
     title = "Hamilton vs Bottas - Mean Racepace Delta per Track",
     subtitle = "Positive Delta (yellow) means Hamilton was faster",
-    y = "Delta in Seconds",
-    x = ""
+    y = "",
+    x = "Delta in Seconds"
   ) +
   scale_fill_manual(values = cbp1) +
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  scale_y_continuous(limits = c(-1.5, 1.5)) +
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(limits = c(-1.5, 1.5)) +
   guides(
     fill = "none"
   )  +
   theme(
     text = element_text(size = 14),
-    plot.margin = margin(t = 1, r = 2, b = 0.5, l = 1, unit = "cm"),
-    axis.text.x = element_text(size = 10)
+    plot.margin = margin(t = 0.5, r = 1, b = 0.5, l = 0.5, unit = "cm"),
+    axis.text = element_text(size = 12)
   )
 
 
 print(g)
 
-ggsave("plots/racepace-mean-ham-bot-delta-per-track-alphabetical.png", width = 9.37, height = 8.0, dpi = 300)
-
-# finalise_plot(plot_name = g,
-#               source = "Source: Gapminder",
-#               save_filepath = "filename_that_my_plot_should_be_saved_to.png",
-#               width_pixels = 640,
-#               height_pixels = 450,
-#               logo_image_path = "hutan-logo.png")
+ggsave("plots/racepace-mean-ham-bot-delta-per-track-alphabetical.png", width = 9.37, height = 11.0, dpi = 300)
 
